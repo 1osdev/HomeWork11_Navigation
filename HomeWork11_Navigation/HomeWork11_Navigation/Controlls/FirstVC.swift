cb5b26339b7ea19cc5e13db62df15a04009f67c1
DMITRY SMIRNOV <smirnoff145@gmail.com>
2022-01-24 20:32:39 +0300

    project finish

--- HomeWork11_Navigation/HomeWork11_Navigation/Controlls/FirstVC.swift
+++ HomeWork11_Navigation/HomeWork11_Navigation/Controlls/FirstVC.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func unwindToFirstVC(_ unwindSegue: UIStoryboardSegue) {
+//        let sourceViewController = unwindSegue.source
+        // Use data from the view controller which initiated the unwind segue
+    }
 
 }
 
